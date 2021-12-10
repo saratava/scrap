8a01b000b8b1a6199894f696fb6991742b26f6de
sara <sara27082011@gmail.com>
2021-12-10 15:44:28 -0300

    scraping/ETL/Data-Mining - Alexa's App - Google-Play-Scraper

--- scrap.py
+++ scrap.py
@@ -1,23 +1,51 @@
-from google_play_scraper import app
+# from google_play_scraper import app
 import pandas as pd
 import numpy as np
 from google_play_scraper import Sort, reviews_all
 
 
-us_reviews = reviews_all(
+br_reviews = reviews_all(
     'com.amazon.dee.app',
-    lang='pt',  # defaults to 0
-    sleep_milliseconds=0,  # defaults to 'en'
+    lang='pt',  # defaults to 'en'
+    sleep_milliseconds=0,  # defaults to 0
     country='br',  # defaults to 'us'
-    sort=Sort.NEWEST,  # defaults to Sort.MOST_RELEVANT
+    sort=Sort.MOST_RELEVANT,  # defaults to Sort.MOST_RELEVANT
 )
 
-df_busu = pd.DataFrame(np.array(us_reviews), columns=['review'])
+df_reviews = pd.DataFrame(np.array(br_reviews), columns=['review'])
+
+df_completa = df_reviews.join(pd.DataFrame(df_reviews.pop('review').tolist()))
+
+df_final = df_completa.drop(columns=['reviewId','userName','userImage'])
+
+filtro_positiva = df_final['score'] >= 4
+df_positiva = df_final[filtro_positiva]
+
+filtro_neutra = df_final['score'] == 3
+df_neutra = df_final[filtro_neutra]
+
+filtro_negativa = df_final['score'] < 3
+df_negativa = df_final[filtro_negativa]
 
 
-df_busu = df_busu.join(pd.DataFrame(df_busu.pop('review').tolist()))
+# df_busu.to_excel("scraping_com_python.xlsx", index=False)
+# df_completa.head()
+# df_final.to_excel("scraping_com_python-limpa.xlsx", index=False)
 
+# print(df_positiva)
+# print()
+# print(df_neutra.to_csv)
+# print()
+# print(df_negativa)
 
-print(df_busu)
+# df_positiva.to_csv("df_positiva.csv", index=False)
+# df_neutra.to_csv("df_neutra.csv", index=False)
+# df_negativa.to_csv("df_negativa.csv", index=False)
+import IPython.core.display
+from pandas_profiling import ProfileReport as pr
 
+profile = pr(df_final, title='Report Geral Reviews Alexa', html={'style':{'full_width':True}})
 
+profile.to_notebook_iframe()
+
+profile.to_file(output_file="/home/sarat/Área de Trabalho/scraping-python-google-play-scraper/Report_Scraping_Alexa.html")
